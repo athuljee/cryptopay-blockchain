362962dfbded16aaa4b75a99ddbba72618e18d66
Athul Jeev S <athuljeev007@gmail.com>
2026-03-10 11:42:41 +0530

    fixed render port

--- server.js
+++ server.js
@@ -326,9 +326,7 @@ app.get("/token-values", (req, res) => {
 });
 
 /* ---------------- SERVER ---------------- */
-
-app.listen(3000, "0.0.0.0", () => {
-
-    console.log("Blockchain server running on port 3000");
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Blockchain server running on port ${PORT}`);
 });
